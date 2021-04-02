361dc7f4bbe8c3e22cce6890dc570d5bd6b33ea6
Денис <d.turchin@bk.ru>
2021-04-03 00:46:19 +0300

    fixed no. 1

--- lab5/5.js
+++ lab5/5.js
@@ -23,6 +23,36 @@
 
 function makeRoute(arr) {
     //code here
+    let result = [];
+    let beg;
+    for (let i = 0; i < arr.length; ++i) {
+        let marker = true;
+        for (let j = 0; j < arr.length; ++j) {
+            // ищем такой вход, который не является
+            // выходом ни для одного из элементов 
+            if (arr[j].to === arr[i].from) {
+                marker = false;
+                break;
+            }
+        }
+        // это будет входная точка графа
+        if (marker === true) {
+            beg = i;
+            break;
+        }
+    }
+    result.push(arr[beg]);
+    let current = beg;
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length; ++j) {
+            if (arr[j].from === arr[current].to) {
+                current = j;
+                result.push(arr[j]);
+                break;
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = makeRoute;
