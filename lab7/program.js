1288754d437d17790cd5e34a6d7f8cd4ebf294cc
Денис <d.turchin@bk.ru>
2021-04-11 15:01:02 +0300

    main done

--- lab7/program.js
+++ lab7/program.js
@@ -1,20 +1,51 @@
 console.log("Script start");
 const empty_city = "City-17"
-const init_city = "Moscow"
+const init_city = "Москва"
+const loading = "Загрузка..."
 
 // данные странички
-let city = init_city;
-let city_field;
-let weather_data;
+let city = init_city;   // выбранный город
+let city_field;         // поле для указания города
+let weather_field;      // поле с текущей погодой
+let weather_data;       // массив данных от сервера
+let weather_pict_field; // поле с картинкой
+let weather_text_field;       // поле с текстом данных о погоде
 
 // сеть
-let requestBase = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=20d168cec6dd572a4b246df42e4646e7';
+let requestBase = 'https://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid=20d168cec6dd572a4b246df42e4646e7';
 let request = new XMLHttpRequest();
 
-function getWeatherForecast() {
-    requestURL = requestBase.replace('{city}', city);
-    //console.log("request: ", requestURL);
+function getUserText(data) {
+    let text;
+    text = '<p"><strong>Температура</strong> {temp}°С</p>\
+    <p>Ощущается {feels_like}°С</p>\
+    <p>мин: {temp_min}°С / макс: {temp_max}°С</p>\
+    </br>\
+    <p><strong>Давление</strong> {pressure} мм.рт.</p>\
+    <p><strong>Влажность</strong> {humidity}%</p>'
+    .replace("{temp}", data['main']['temp'])
+    .replace("{feels_like}", data['main']['feels_like'])
+    .replace("{temp_min}", data['main']['temp_min'])
+    .replace("{temp_max}", data['main']['temp_max'])
+    .replace("{pressure}", data['main']['pressure'])
+    .replace("{humidity}", data['main']['humidity']);
+    return text;
+    // text = '<div class="d-flex"><strong>Температура</strong> {temp}°С</div></br>\
+    // <div class="d-flex">Ощущается {feels_like}°С</div></br>\
+    // <div class="d-flex">мин: {temp_min}°С макс: {temp_max}°С</div></br>\
+    // </br>\
+    // <div class="d-flex"><strong>Давление</strong> {pressure} мм.рт.</div></br>\
+    // <div class="d-flex"><strong>Влажность</strong> {humidity}%</div></br>'
+}
 
+function getWeatherForecast() {
+    weather_field = document.getElementById("current weather");
+    weather_field.innerHTML = loading;
+    weather_pict_field = document.getElementById("weather_pict");
+    weather_pict_field.innerHTML = '<img src="img/loading.png"></img>';
+    weather_text_field = document.getElementById("data");
+    
+    requestURL = requestBase.replace('{city}', city);
     request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
@@ -22,7 +53,16 @@ function getWeatherForecast() {
 
     request.onload = function() {
         weather_data = request.response;
-        console.log("got weather data | weather in " + city + "  is: " + weather_data['weather'][0]['main']);
+        console.log("got weather data | weather_main: " + weather_data['weather'][0]['main']);
+        console.log("got weather data | weather_description: " + weather_data['weather'][0]['description']);
+        console.log("got weather data | weather_icon: " + weather_data['weather'][0]['icon']);
+        console.log("got weather data | name: " + weather_data['name']);
+
+        //weather_data = {weather: [{main: "test ok"}]};
+        //weather_pict_field.innerHTML = 'src="img/test.png"';
+        weather_field.innerHTML = weather_data['weather'][0]['description'];
+        weather_pict_field.innerHTML = '<img src="img/{icon}.png">'.replace("{icon}",  weather_data['weather'][0]["icon"]);
+        weather_text_field.innerHTML = getUserText(weather_data);
     }
 }
 
@@ -43,6 +83,6 @@ function getCity() {
     city_field.innerHTML = city;
 }
 
-setTimeout(getCity(), 3000);
-//getWeatherForecast();
+getCity();
+getWeatherForecast();
 console.log("Script end");
